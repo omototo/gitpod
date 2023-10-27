94749e995788c5186c45c17359ea0373775d1c70
Alexander Pinsker <pinskerster@gmail.com>
2023-10-27 14:13:48 +0000

    test

--- test/load_test.sh
+++ test/load_test.sh
@@ -42,10 +42,41 @@ figlet -c "Go!"
 # The 'sleep 1' here gives the user a moment to see the 'Go!' text before the screen clears for the 'wrk' command.
 sleep 1
 
+
 # Clear the screen
 clear
+# Use figlet to create large letters. We center the text with '-c'
+echo 'wrk -t2 -c200 -d120s "https://proxy.alexp-aws-test.gitpod.cloud/stats" -c wrk.lua'
 
-# Execute the wrk command
-wrk -t02 -c200 -d120s "https://haproxy-lb-1563643291.eu-central-1.elb.amazonaws.com/" -s wrk.lua
 
-# Script ends here
+# Execute the wrk command and capture its output
+wrk_output=$(wrk -t02 -c200 -d120s "https://haproxy-lb-1563643291.eu-central-1.elb.amazonaws.com/" -s wrk.lua)
+
+# Assuming that 'wrk' output contains 'Requests/sec' and 'Non-2xx or 3xx responses:' for error count.
+# The awk command is used to parse these values out of the 'wrk' output. This parsing depends on your actual 'wrk' output.
+
+# Extract requests per second
+req_per_sec=$(echo "$wrk_output" | awk '/Requests\/sec:/ {print $2}')
+
+# Extract error count
+error_count=$(echo "$wrk_output" | awk '/Non-2xx or 3xx responses:/ {print $NF}')
+
+# Extract average latency (you may need to adjust based on your 'wrk' version's output format)
+avg_latency=$(echo "$wrk_output" | awk '/Latency/ {print $2}')
+
+# Extract some other metric; replace 'OtherMetric' with the actual metric's label in 'wrk' output
+other_metric=$(echo "$wrk_output" | awk '/OtherMetric/ {print $2}')
+
+# Clear the screen before showing the new information
+clear
+
+# Show the req/s value using figlet
+figlet -c "Req/sec: $req_per_sec"
+
+# Show the error count using figlet
+figlet -c "Errors: $error_count" 
+
+# Show the average latency using figlet
+figlet -c "Avg Latency: $avg_latency"
+
+
--- test/wrk.lua
+++ test/wrk.lua
@@ -2,16 +2,14 @@ wrk.path  = "/flip-image/test?bucket=hello-demo-images-bucket&key=input/image-50
 wrk.method = "GET"
 
 logfile = io.open("wrk.log", "w");
-local cnt = 0;
 
 response = function(status, header, body)
      logfile:write("status:" .. status .. "\n");
-     cnt = cnt + 1;
      logfile:write("status:" .. status .. "\n" .. body .. "\n-------------------------------------------------\n");
 end
 
 done = function(summary, latency, requests)
-     logfile:write("------------- SUMMARY -------------\n")
-     print("Response count: ", cnt)
+     logfile:write("------------- END -------------\n")
+
      logfile.close();
 end
